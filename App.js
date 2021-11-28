4e86c2b71ac455c071990ad0e50621cb681020c3
fsiqueirag <64283507+fsiqueirag@users.noreply.github.com>
2021-11-28 20:14:00 -0300

    Revert "Correccion about, mapview apikey y zoom mapview"

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { LogBox } from "react-native";
-import Navigation from "./app/navigations/Navigation";
-
-LogBox.ignoreAllLogs();
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
